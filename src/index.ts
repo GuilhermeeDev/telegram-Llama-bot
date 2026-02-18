324d1cc02169e9c238241678f80e96d76bfb6f4d
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2026-02-18 16:20:44 -0400

    (update) Adicionando função de /perguntar; Adicionando função /help

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import { Bot }from "grammy";
 import dotenv from 'dotenv';
-
+import {processQuestion} from "./functions.js"
 dotenv.config();
 
 const bot = new Bot(process.env.BOT_TOKEN!);
 
-bot.on("message", (ctx) => ctx.reply("Hi there!"));
+bot.on("message", (ctx) => ctx.reply("--- Bot Iniciado ---"));
 
+bot.command("perguntar",async (ctx) => {
+    const question = ctx.msg.text.substring(10); // Remove "/perguntar "
+    const response = await processQuestion(question);
+    ctx.reply(response);
+});
+
+bot.command("help",(ctx) => ctx.reply("Use /perguntar para fazer uma pergunta"));
 bot.start();
