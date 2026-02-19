01923bc4b1b0dbf98c2a097652e67a363d86b623
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2026-02-19 15:15:48 -0400

    (fix) Corrigindo erro de importação de modelo .guff para a inferencia

--- src/functions.ts
+++ src/functions.ts
@@ -1,16 +1,15 @@
-import {fileURLToPath} from "url";
 import {getLlama, LlamaChatSession} from "node-llama-cpp";
-import path from "path";
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config();
 
 async function processQuestion(question: string) {
-    const __dirname = path.dirname(fileURLToPath(import.meta.url));
     const llama = await getLlama();
-    const model = await llama.loadModel({
-    modelPath: path.join(__dirname, "models", "Llama-3.2-1B-Instruct.Q4_K_M.gguf")});
+    const model = await llama.loadModel({modelPath: path.resolve(process.env.MODEL_PATH!)});
     const context = await model.createContext();
     const session = new LlamaChatSession({contextSequence: context.getSequence()});
-    const resul = await session.prompt(question);
-    return resul;
+    const result = await session.prompt(question);
+    return result;
 }
 
 export {processQuestion}
